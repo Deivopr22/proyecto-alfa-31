fe8ffbdd1e533ab4820b9710c0b51311c1b8d51f
Deivis P. <deivisp563@gmail.com>
2026-09-04 16:03:51 -0400

    Importamos iconify y agregamos detalles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
+<script>
+    import Icon from '@iconify/svelte';
+</script>
+
 <section>
     <h1>Creating digital experiences through intentional design</h1>
         <p>A multidisciplinary design studio focused on editorial clarity, structural typography, and high-end minimalism. We strip away the unnecesary to reveal what truly matters.</p>
     <button>View more</button>
+    <Icon icon="mdi:arrow-right" />
 </section>
