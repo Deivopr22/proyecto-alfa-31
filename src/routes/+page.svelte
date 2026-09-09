dfdd737f65a8d3c52de4f998949e7f5e3c06fb45
Deivis P. <deivisp563@gmail.com>
2026-09-09 17:02:25 -0400

    Cambios al botón

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,12 @@
 </script>
 
 <section class = "py-54">
-    <h1 class ="flex justify-start font-bold gap-1.5">Creating digital experiences through intentional design</h1>
+    <h1 class ="flex justify-start font-bold gap-1.5 text-7xl">Creating digital experiences through intentional design</h1>
         <p class = "flex justify-start font-semibold gap-3.5">A multidisciplinary design studio focused on editorial clarity, structural typography, and high-end minimalism. We strip away the unnecesary to reveal what truly matters.</p>
     <div>
-        <button class = "bg-black text-white px-4 py-2 rounded gap-4">View work</button>
-        <Icon icon="at-icons:arrow-turn-up-right" />
+        <button class = "bg-black text-white px-4 py-2 rounded gap-4 flex justify-center items-center">
+            <Icon icon="at-icons:arrow-turn-up-right" class=""/>
+            View work
+        </button>
     </div>
 </section>
